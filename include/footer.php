98f21c8853f86935d6aa7b158eedb99572c44be8
tigerkig <98365520+tigerkig@users.noreply.github.com>
2022-08-19 10:38:44 +0900

    first commit

---
+++ include/footer.php
@@ -0,0 +1,89 @@
+<footer class="section-footer bg-primary footer-dark shadow mt-4">
+    <div class="container">
+        <section class="footer-main py-5">
+            <div class="row">
+                <aside class="col-lg-4 col-12">
+                    <article class="me-lg-4 mb-4">
+                        <h6 class="title"><?= $_lang['contact_us'] ?></h6>
+                        <ul class="list-icon">
+                            <li class="mb-2"> <i class="icon fa fa-map-marker"></i> Moscow, Russia, Fake Street, <br> Area name, PO 10021 </li>
+                            <li class="mb-2"> <i class="icon fa fa-phone"></i> (800) 060-0730, (800) 060-0730 </li>
+                            <li class="mb-2"> <i class="icon fa fa-envelope"></i> info@mywebsite.com </li>
+                        </ul>
+                    </article>
+                </aside>
+                <!-- col end.// -->
+                <aside class="col-lg col-6">
+                    <h6 class="title"><?= $_lang['company'] ?></h6>
+                    <ul class="list-menu mb-4">
+                        <li> <a href="#"> <?= $_lang['about_us'] ?> </a> </li>
+                        <li> <a href="#"> <?= $_lang['career'] ?> </a> </li>
+                        <li> <a href="#"> <?= $_lang['find_a_store'] ?> </a> </li>
+                        <li> <a href="#"> <?= $_lang['rules_and_terms'] ?> </a> </li>
+                        <li> <a href="#"> <?= $_lang['sitemap'] ?> </a> </li>
+                    </ul>
+                </aside>
+                <!-- col end.// -->
+                <aside class="col-lg col-6">
+                    <h6 class="title"> <?= $_lang['services'] ?> </h6>
+                    <ul class="list-menu mb-4">
+                        <li> <a href="#"> <?= $_lang['help_center'] ?> </a> </li>
+                        <li> <a href="#"> <?= $_lang['money_refund'] ?> </a> </li>
+                        <li> <a href="#"> <?= $_lang['order_status'] ?></a> </li>
+                        <li> <a href="#"> <?= $_lang['shipping_info'] ?> </a> </li>
+                        <li> <a href="#"> <?= $_lang['open_dispute'] ?> </a> </li>
+                    </ul>
+                </aside>
+                <!-- col end.// -->
+                <aside class="col-lg col-6">
+                    <h6 class="title"> <?= $_lang['user_center'] ?> </h6>
+                    <ul class="list-menu mb-4">
+                        <li> <a href="#"> <?= $_lang['register'] ?> </a> </li>
+                        <li> <a href="#"> <?= $_lang['signin'] ?> </a> </li>
+                        <li> <a href="#"> <?= $_lang['restore_account'] ?> </a> </li>
+                        <li> <a href="#"> <?= $_lang['track_order'] ?> </a> </li>
+                    </ul>
+                </aside>
+                <!-- col end.// -->
+                <aside class="col-lg col-6">
+                    <h6 class="title"><?= $_lang['soical'] ?></h6>
+                    <ul class="list-icon list-menu mb-4">
+                        <li>
+                            <a href="#"> <i class="icon fab fa-facebook"></i> <?= $_lang['facebook'] ?> </a>
+                        </li>
+                        <li>
+                            <a href="#"> <i class="icon fab fa-twitter"></i> <?= $_lang['twitter'] ?> </a>
+                        </li>
+                        <li>
+                            <a href="#"> <i class="icon fab fa-instagram"></i> <?= $_lang['instagram'] ?> </a>
+                        </li>
+                        <li>
+                            <a href="#"> <i class="icon fab fa-youtube"></i> <?= $_lang['youtube'] ?> </a>
+                        </li>
+                    </ul>
+                </aside>
+            </div>
+            <!-- row end.// -->
+        </section>
+        <hr class="my-0">
+        <section class="footer-bottom d-flex justify-content-between">
+            <p class="text-white-50 mb-0"> © 2021 theSharePage. All rights reserved. </p>
+            <div> <i class="fab fa-lg fa-cc-visa"></i>
+                <i class="fab fa-lg fa-cc-amex"></i>
+                <i class="fab fa-lg fa-cc-mastercard"></i>
+                <i class="fab fa-lg fa-cc-paypal"></i>
+            </div>
+        </section>
+    </div>
+    <!-- container end.// -->
+</footer>
+
+<!-- JS -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="../assets/js/bootstrap.bundle.min.js?v=3" type="text/javascript"></script>
+<script src="../assets/js/script.js?v=3" type="text/javascript"></script>
+
+<noscript>
+    <div><img src="https://mc.yandex.ru/watch/87410011" style="position:absolute; left:-9999px;" alt="" /></div>
+</noscript>
+<!-- /Yandex.Metrika counter -->
